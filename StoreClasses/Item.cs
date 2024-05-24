72c0f935a3df320dfb0345afac2d9b8394f26c30
AleksandrJefremov <aleksandr.jefremov@outlook.com>
2024-05-24 21:03:40 +0200

    fresh produce

--- StoreClasses/Item.cs
+++ StoreClasses/Item.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace StoreClasses
@@ -68,5 +69,21 @@ namespace StoreClasses
                 SetPrice(0);
             }
         }
+    }
+    internal class FreshProduce : PerishableItem
+    {
+        // Properties
+        private string farm;
+
+        // Constructor
+        public FreshProduce(string itemId, string name, double price, bool perished, string origin)
+            : base(itemId, name, price, perished)
+        {
+            farm = origin;
+        }
+
+        // get set
+        public string GetFarm() => farm;
+        public void SetFarm(string value) => farm = value;
     }
 }
